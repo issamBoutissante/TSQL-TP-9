93863b20fdb67dce57a95b633837fe3fbd08d94e
issamBoutissante <boutissante.issam.dev@gmail.com>
2021-03-18 20:34:38 +0100

    done

--- Gestion de Commande Queries.sql
+++ Gestion de Commande Queries.sql
@@ -13,10 +13,48 @@ print dbo.premierLittreEnMajuscule('bOUtissante')
 
 
 --4-	Créer une fonction qui permet de lire le nom d’une table puis afficher
---les noms des colonnes de cette table. 
+-- les noms des colonnes de cette table. 
 create function collonnesDe(@tableNom varchar(30))
+returns  table
+as
+	 return select COLUMN_NAME from Information_schema.columns where table_name=@tableNom
+select * from collonnesDe('LigneCommande')
+
+-- 5-	Créer une fonction nommée F_ListeArticles qui return la liste
+-- des articles d'une commande dont le numéro est donné en paramètre 
+create function F_ListeArticles(@numCom int)
+returns table
+as
+  return select l.numArt,qteCommandee,puart,qtEenStock,seuilMinimum, seuilMaximum 
+  from LigneCommande l join Article a
+  on l.numArt=a.numArt
+  where numCom=@numCom
+select * from F_ListeArticles(10)
+
+-- 6-	Créer une procédure stockée nommée PS_MontantArticles, 
+-- qui utilise une fonction de calcul du montant, pour afficher la liste des lignes 
+-- d’articles d'une commande dont le numéro est donné en paramètre.
+
+create procedure PS_MontantArticles(@numCom int)
+as 
+begin
+  print dbo.calMontant(@numCom)
+  select l.numArt,qteCommandee,puart,qtEenStock,seuilMinimum, seuilMaximum  from LigneCommande l join Article a
+  on l.numArt=a.numArt
+  where numCom=@numCom
+end 
+exec PS_MontantArticles 10
+
+--7-	Créer une fonction qui retourne une table contenant les détails d’une commande
+create function GetCommandDetails(@numCom int)
+returns @details table(numArt int,puart decimal,qtcommndee int,montant decimal)
 as
 begin
-	 declare @collonnes
-	  return(select top 0 * from Article )
+   insert into @details select l.numArt,puart,qteCommandee,(qteCommandee*puart)
+   from LigneCommande l join Article a
+   on l.numArt=a.numArt
+   where numCom=@numCom
+   return
 end
+
+select * from GetCommandDetails(10)
